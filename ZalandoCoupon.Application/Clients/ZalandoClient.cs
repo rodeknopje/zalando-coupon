6e0b0d85b5d78dfd7be87969a4d688b1999791ae
rodeknopje <mn.stokhorst@gmail.com>
2022-06-03 21:08:21 +0200

    1secmail api is broken, use mailslurp instead

--- ZalandoCoupon.Application/Clients/ZalandoClient.cs
+++ ZalandoCoupon.Application/Clients/ZalandoClient.cs
@@ -13,10 +13,8 @@ public class ZalandoClient
 
     public async Task RegisterToNewsletterAsync(string email)
     {
-        var content = $"[{{\"id\":\"f321f59294a4ffd369951dc5d8f92b801cb7c3c7302de9e5118b3569416c844f\",\"variables\":{{\"input\":{{\"email\":\"{email}\",\"preference\":{{\"category\":\"MEN\",\"topics\":[{{\"id\":\"item_alerts\",\"isEnabled\":true}},{{\"id\":\"recommendations\",\"isEnabled\":true}},{{\"id\":\"fashion_fix\",\"isEnabled\":true}},{{\"id\":\"follow_brand\",\"isEnabled\":true}},{{\"id\":\"subscription_confirmations\",\"isEnabled\":true}},{{\"id\":\"survey\",\"isEnabled\":true}},{{\"id\":\"offers_sales\",\"isEnabled\":true}}]}},\"referrer\":\"nl_subscription_page\",\"clientMutationId\":\"1630664083241\"}}}}}}]";
-            
+        var content = "[{\"id\":\"f321f59294a4ffd369951dc5d8f92b801cb7c3c7302de9e5118b3569416c844f\",\"variables\":{\"input\":{\"email\":\""+email+"\",\"preference\":{\"category\":\"MEN\",\"topics\":[{\"id\":\"recommendations\",\"isEnabled\":true},{\"id\":\"item_alerts\",\"isEnabled\":false},{\"id\":\"fashion_fix\",\"isEnabled\":false},{\"id\":\"follow_brand\",\"isEnabled\":false},{\"id\":\"subscription_confirmations\",\"isEnabled\":false},{\"id\":\"offers_sales\",\"isEnabled\":false},{\"id\":\"survey\",\"isEnabled\":false}]},\"referrer\":\"nl_subscription_page\",\"clientMutationId\":\"1654274946563\"}}}]";
         var response = await _client.PostAsync("graphql", new StringContent(content));
-
         response.EnsureSuccessStatusCode();
     }
 
--- ZalandoCoupon.Application/Program.cs
+++ ZalandoCoupon.Application/Program.cs
@@ -1,57 +1,47 @@
 ﻿using System.Text.RegularExpressions;
+using mailslurp.Api;
+using mailslurp.Client;
 using ZalandoCoupon.Application.Clients;
-
-const int maxTries = 15;
 
 var couponRegex = new Regex(">(NL[A-Z\\d]{8})<", RegexOptions.Compiled);
 
-var mailClient = new MailClient();
-var zalandoClient = new ZalandoClient();
+var configuration = new Configuration();
 
-Console.WriteLine("[1] Fetching email address");
-var address = await mailClient.GetRandomEmailAddressAsync();
+var mailSlurpApi = Environment.GetEnvironmentVariable("MailSlurpApi");
 
-Console.WriteLine("[2] Subscribing to zalando newsletter");
-await zalandoClient.RegisterToNewsletterAsync(address);
-
-Console.Write($"\r[3] Retrieving mails {GetAttempts(0)}");
-
-for (var i = 0; i < maxTries; i++)
+if (mailSlurpApi is null)
 {
-    await Task.Delay(TimeSpan.FromSeconds(2));
-    
-    var mails = await mailClient.GetMailsAsync(address);
-
-    Console.Write($"\r[3] Retrieving mails {GetAttempts(i+1)}");
-    
-    foreach (var mail in mails)
-    {
-        var message = await mailClient.GetMessageAsync(address, mail.Id);
-
-        var match = couponRegex.Match(message.HtmlBody);
-
-        if (match.Success)
-        {
-            Console.WriteLine($"\r[3] Retrieving mails {GetAttempts(maxTries)}");
-
-            Console.Write($"[4] Coupon: ");
-            var defaultColor = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(match.Groups[1]);
-            Console.ForegroundColor = defaultColor;
-            Console.WriteLine();
-            Console.WriteLine("Press enter to exit");
-            Console.ReadLine();
-            return;
-        }
-    }
+    Console.WriteLine("No MailSlurp api key provided");
+    Console.ReadKey();
+    return;
 }
 
-Console.WriteLine("[4] No coupon found");
-Console.WriteLine();
-Console.WriteLine("Press enter to exit");
+configuration.ApiKey.Add("x-api-key", mailSlurpApi);
 
-string GetAttempts(int current)
+var mailSlurp = new InboxControllerApi(configuration);
+
+Console.WriteLine("[1] Creating inbox.");
+var inbox = await mailSlurp.CreateInboxAsync(expiresAt: DateTime.Now.AddMinutes(3));
+
+Console.WriteLine($"[3] Signing up for zalando newsletter.");
+await new ZalandoClient().RegisterToNewsletterAsync(inbox.EmailAddress);
+
+var waitForInstance = new WaitForControllerApi(configuration);
+
+Console.WriteLine("[4] Waiting for email.");
+
+var email = await waitForInstance.WaitForLatestEmailAsync(inbox.Id, TimeSpan.FromMinutes(2).Milliseconds, true) ?? throw new Exception("No email received");
+
+var match = couponRegex.Match(email.Body);
+
+if (match.Success)
 {
-    return $"[{new string('#',current)}{new string('.',maxTries-current)}]";
+    Console.Write($"[5] Coupon: ");
+    var defaultColor = Console.ForegroundColor;
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(match.Groups[1]);
+    Console.ForegroundColor = defaultColor;
+    Console.WriteLine();
+    Console.WriteLine("Press enter to exit");
+    Console.ReadLine();
 }
